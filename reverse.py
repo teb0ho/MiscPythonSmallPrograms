88a8984530530b0b139d56a1f4380a8f3e2e34ae
Teboho Mphure <tebohomphure@gmail.com>
2018-03-14 10:33:00 +0200

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,5 +1,5 @@
 def reverse(text):
-  "my own function to reverse any text provided"
+  """my own function to reverse any text provided"""
   rev = ""
   for i in range(len(text) - 1, -1, -1):
     rev += text[i]
